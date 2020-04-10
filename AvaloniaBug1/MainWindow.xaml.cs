136aa5d13580af63f5bb1f0694e2f4f584486285
Alexander Gnauck <gnauck@gmail.com>
2020-04-10 20:15:36 +0200

    init

---
+++ AvaloniaBug1/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaBug1
+{
+    using System.IO;
+    using Avalonia.Media;
+    using Avalonia.Media.Imaging;
+
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+#if DEBUG
+            // this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
